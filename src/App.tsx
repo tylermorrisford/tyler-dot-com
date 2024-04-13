0d37e303af5b2864c0f0b55859c322ffcd45431f
Tyler Ford <tyler.ford@commercetools.com>
2024-04-13 15:51:37 -0600

    feat(app): new landing page

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>hi, I'm tyler</h1>
+      <p>
+        this url should point to a wordpress site but I honestly cannot remember
+        where its hosted :/{" "}
+      </p>
+      <p>
+        Until I figure that out (lol) check out{" "}
+        <a href="https://teef.dev" target="_blank" rel="noopener noreferrer">
+          teef.dev
+        </a>{" "}
+        and/or{" "}
+        <a href="https://teef.blog" target="_blank" rel="noopener noreferrer">
+          teef.blog
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default App;
